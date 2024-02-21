c4b0d814c15cb4c898b472caad5141683269c835
kunihiko <127746548+kunipico@users.noreply.github.com>
2024-02-21 23:56:38 +0900

    [CREATE] Simple login page

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 
 ###################
 # Initialization  #
 ###################
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+@app.route('/')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port='8080')
